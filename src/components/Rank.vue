bfeedb070e72c230889f9d87b9e79d4f43e7c1db
MaskerT <1072960055@qq.com>
2022-11-05 20:50:46 +0800

    rank条形图提交

---
+++ src/components/Rank.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="rank_ref"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartInstane: null,
+      allData: null, // 从服务器中获取的所有数据
+      startValue: 0, // 区域缩放的起点值
+      endValue: 9, // 区域缩放的重点值
+      timerId: null // 定时器标识
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    this.screenAdapter()
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.screenAdapter)
+    clearInterval(this.timerId)
+  },
+  methods: {
+    initChart() {
+      this.chartInstane = this.$echarts.init(this.$refs.rank_ref, 'chalk')
+      const initOption = {
+        title: {
+          text: '▎地区销售排行',
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '40%',
+          left: '5%',
+          right: '5%',
+          bottom: '5%',
+          containLabel: true
+        },
+        tooltip: {
+          show: true
+        },
+        xAxis: {
+          type: 'category'
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            type: 'bar'
+          }
+        ]
+      }
+      this.chartInstane.setOption(initOption)
+      this.chartInstane.on('mouseover', () => {
+        clearInterval(this.timerId)
+      })
+      this.chartInstane.on('mouseout', () => {
+        this.startInterval()
+      })
+    },
+    async getData() {
+      // await this.$http.get()
+      // 对allData进行赋值
+      const { data: ret } = await this.$http.get('rank')
+      this.allData = ret
+      // 对alldata中的元素从大到小排序
+      this.allData.sort((a, b) => {
+        return b.value - a.value
+      })
+      this.updateChart()
+      this.startInterval()
+    },
+    updateChart() {
+      const colorArr = [
+        ['#0BA82C', '#4FF778'],
+        ['#2E72BF', '#23E5E5'],
+        ['#5052EE', '#AB6EE5']
+      ]
+      // 所有省份的数组
+      const provinceArr = this.allData.map(item => {
+        return item.name
+      })
+      // 所有省份对应的销售金额
+      const valueArr = this.allData.map(item => {
+        return item.value
+      })
+      const dataOption = {
+        xAxis: {
+          data: provinceArr
+        },
+        dataZoom: {
+          show: false,
+          startValue: this.startValue,
+          endValue: this.endValue
+        },
+        series: [
+          {
+            data: valueArr,
+            itemStyle: {
+              color: arg => {
+                let targetColorArr = null
+                if (arg.value > 300) {
+                  targetColorArr = colorArr[0]
+                } else if (arg.value > 200) {
+                  targetColorArr = colorArr[1]
+                } else {
+                  targetColorArr = colorArr[2]
+                }
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  {
+                    offset: 0,
+                    color: targetColorArr[0]
+
+                  },
+                  {
+                    offset: 1,
+                    color: targetColorArr[1]
+                  }
+                ])
+              }
+            }
+          }
+        ]
+      }
+      this.chartInstane.setOption(dataOption)
+    },
+    screenAdapter() {
+      const titleFontSize = this.$refs.rank_ref.offsetWidth / 100 * 3.6
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0]
+            }
+          }
+        ]
+      }
+      this.chartInstane.setOption(adapterOption)
+      this.chartInstane.resize()
+    },
+    startInterval() {
+      if (this.timerId) {
+        clearInterval(this.timerId)
+      }
+      this.timerId = setInterval(() => {
+        this.startValue++
+        this.endValue++
+        if (this.endValue > this.allData.length - 1) {
+          this.startValue = 0
+          this.endValue = 9
+        }
+        this.updateChart()
+      }, 2000)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/RankPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="com-page">
+    <Rank></Rank>
+  </div>
+</template>
+
+<script>
+import Rank from '@/components/Rank'
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {
+    Rank
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
